0f8ca2a37d201b854ba3cd3c7e40ffda7752095b
mokomintstore-sketch <mokomintstore@gmail.com>
2026-04-17 22:33:30 +0530

    added category images

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,10 +1,12 @@
 import HomeCategories from "../components/HomeCategories";
 import HomeHero from "../components/HomeHero";
+import HomeFeatures from "../components/HomFeatures";
 
 export default function HomePage() {
   return (
     <>
       <HomeHero />
+      <HomeFeatures/>
       <HomeCategories />
     </>
   );
