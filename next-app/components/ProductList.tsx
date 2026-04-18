b9cde8dde7232becd32d1b5faf35af5b804c511f
mokomintstore-sketch <mokomintstore@gmail.com>
2026-04-18 11:05:54 +0530

    corrected responsiveness

--- next-app/components/ProductList.tsx
+++ next-app/components/ProductList.tsx
@@ -8,7 +8,9 @@ export default function ProductList({ products }: ProductListProps) {
   return (
     <div className="row pb-3">
       {products.map((product) => (
-        <ProductCard key={product.id} {...product} />
+        <div key={product.id} className="col-lg-4 col-md-6 col-sm-12 pb-1">
+          <ProductCard {...product} />
+        </div>
       ))}
     </div>
   );
